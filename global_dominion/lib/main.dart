8e427fca8d9c6a622726320ce35650746914c402
Hajinwoo28 <9599teaandcoffeeshop@gmail.com>
2026-06-18 11:03:25 +0800

    Fix duplicate app.run

---
+++ global_dominion/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GlobalDominion());
+}
+
+class GlobalDominion extends StatelessWidget {
+  const GlobalDominion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Global Dominion',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+  width: double.infinity,
+  decoration: const BoxDecoration(
+  image: DecorationImage(
+    image: AssetImage('assets/images/world_map.jpg'),
+    fit: BoxFit.cover,
+  ),
+),
+  child: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'GLOBAL DOMINION',
+          style: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+  ),
+)
+    );
+  }
+}
